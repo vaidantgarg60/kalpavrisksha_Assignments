678b51f6d2ad0a3ed8ed409eabc0a6ed2abc7f27
vaidant garg <146158704+vaidantgarg60@users.noreply.github.com>
2025-12-26 14:34:59 +0530

    Add shared memory IPC mechanism for sorting
    Implement shared memory IPC for sorting an array.

--- Assignment_9/ sharedMemoryIPCMechanism/sharedMemoryIPC.c
+++ Assignment_9/ sharedMemoryIPCMechanism/sharedMemoryIPC.c
@@ -1,1 +1,43 @@
-1000
+#include <stdio.h>
+#include <sys/shm.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+void sort(int arr[], int size) {
+    for(int index = 0; index < size - 1; index++) {
+        for(int iterator = 0; iterator < size - index - 1; iterator++) {
+            if(arr[iterator] > arr[iterator + 1]) {
+                int temp = arr[iterator];
+                arr[iterator] = arr[iterator + 1];
+                arr[iterator + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int shmId = shmget(IPC_PRIVATE, 5 * sizeof(int), 0666 | IPC_CREAT);
+    int* arr = shmat(shmId, NULL, 0);
+
+    int data[] = {9, 5, 2, 7, 1};
+
+    for(int index = 0; index < 5; index++) {
+        arr[index] = data[index];
+    }
+
+    if(fork() == 0) {
+        sort(arr, 5);
+        shmdt(arr);
+    } else {
+        wait(NULL);
+        printf("Sorted array: \n");
+
+        for(int index = 0; index < 5; index++) {
+            printf("%d ", arr[index]);
+        }
+        
+        shmctl(shmId, IPC_RMID, NULL);
+    }
+
+    return 0;
+}
